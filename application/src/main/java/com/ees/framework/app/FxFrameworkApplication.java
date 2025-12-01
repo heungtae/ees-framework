5593a5b4f31a7eefe4ffc7eefdd4c96fee686179
heungtae <heungtae@gmail.com>
2025-12-01 20:51:39 +0900

    chore: add spring boot application module

---
+++ application/src/main/java/com/ees/framework/app/FxFrameworkApplication.java
@@ -0,0 +1,15 @@
+package com.ees.framework.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * EES Framework를 실행 가능한 Spring Boot 애플리케이션으로 기동하는 진입점.
+ */
+@SpringBootApplication
+public class FxFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FxFrameworkApplication.class, args);
+    }
+}
